d7221b4c7b4a4d1c65e6619a11d510ed34a97181
linchen <clin366@gatech.edu>
2020-09-23 03:46:25 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='CMFN model for imputing missing values in multimodal sequential data',
+    author='Emory University',
+    license='MIT',
+)
